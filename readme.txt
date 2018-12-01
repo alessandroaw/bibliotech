05fc4172d6d0b6820de9c99574d9ebecb66d2e84
theRealSandro <assandroa95@gmail.com>
2018-12-01 12:18:52 +0700

    mvp for asisten

---
+++ readme.txt
@@ -0,0 +1,5 @@
+to start the server use "npm start"
+there are no initial anggota account so you need to make sign up one first
+there is initial pustakawan account:
+  username: "asisten"
+  password:" asdfasdf"
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Bibliotech';
+// const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Bibliotech';
+const MONGODB_URI = 'mongodb://asisten:bibliotech3@ds123444.mlab.com:23444/bibliotech';
 
 mongoose.Promise = global.Promise;
 mongoose.connect(MONGODB_URI);
